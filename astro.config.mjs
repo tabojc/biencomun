27173ae035aa999ceaf450a582c46babb67a9c8c
Juan Taborda <tabordajuan@gmail.com>
2024-11-27 12:17:54 -0400

    feat: add hibrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://biencomun.genteintegra.com",
-  base: "/"
+  base: "/",
+  output: 'hybrid',
 });
